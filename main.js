55babfc2e63031329480c56dc45e0c2de0e1b3f3
KachmarAN <bilux.teplo@gmail.com>
2025-02-19 21:02:20 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -2,16 +2,13 @@
 
 // Введіть п'ятизначне число
 let number = parseInt(prompt("Введіть п'ятизначне число:"));
-
 // Отримуємо цифри
 let digit1 = number / 10000 - (number / 10000) % 1;
 let digit2 = (number % 10000) / 1000 - ((number % 10000) / 1000) % 1;
 let digit3 = (number % 1000) / 100 - ((number % 1000) / 100) % 1;
 let digit4 = (number % 100) / 10 - ((number % 100) / 10) % 1;
 let digit5 = number % 10;
-
 alert(`Digits: ${digit1} ${digit2} ${digit3} ${digit4} ${digit5}`);
-
 
 
 let number1 = prompt("Введіть тризначне число:");
