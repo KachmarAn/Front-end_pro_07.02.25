2f194b376b762185a5ebde2451feaea082fa4a58
KachmarAN <bilux.teplo@gmail.com>
2025-02-19 20:05:18 +0200

    modified:   main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+let str = 'Рядок'; //string
+let age = 40; // number
+let isDeveloper = true; //boolean
+let power = 9999999999999999n; //bigint
+let selectedColor = null; //null
+
+
+console.log (str)
+console.log (age)
+console.log (isDeveloper)
+console.log (power)
+console.log (selectedColor)
