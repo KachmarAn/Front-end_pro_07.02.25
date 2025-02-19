3e7a2c0f680a4c09a6c6b9c1889338c1a171473e
KachmarAN <bilux.teplo@gmail.com>
2025-02-19 20:18:39 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+'use strict'
+
+// Введіть п'ятизначне число
+let number = parseInt(prompt("Введіть п'ятизначне число:"));
+
+// Отримуємо цифри
+let digit1 = number / 10000 - (number / 10000) % 1;
+let digit2 = (number % 10000) / 1000 - ((number % 10000) / 1000) % 1;
+let digit3 = (number % 1000) / 100 - ((number % 1000) / 100) % 1;
+let digit4 = (number % 100) / 10 - ((number % 100) / 10) % 1;
+let digit5 = number % 10;
+
+alert(`Digits: ${digit1} ${digit2} ${digit3} ${digit4} ${digit5}`);
