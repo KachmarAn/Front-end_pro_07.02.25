2fb996346f0aab101ae2f272135a962c72adae3f
KachmarAN <bilux.teplo@gmail.com>
2025-02-19 20:41:35 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ let power = 9999999999999999n; //bigint
 let selectedColor = null; //null
 
 
-console.log (str)
-console.log (age)
-console.log (isDeveloper)
-console.log (power)
-console.log (selectedColor)
+console.log(str, '-', typeof str); // "Рядок - string"
+console.log(age, '-', typeof age); // "40 - number"
+console.log(isDeveloper, '-', typeof isDeveloper); // "true - boolean"
+console.log(power, '-', typeof power); // "9999999999999999n - bigint"
+console.log(selectedColor, '-', typeof selectedColor); // "null - object" (особливість JS)
