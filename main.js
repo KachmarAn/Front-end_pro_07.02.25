4b9c355ebf4726b7eb7969567a059284dfcd7d53
KachmarAN <bilux.teplo@gmail.com>
2025-02-19 20:30:36 +0200

    commit

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+'use strict'
+
+let userName = prompt("Введіть ваше ім'я:");
+let year = prompt("Введіть ваш рік народження:");
+let city = prompt("Введіть ваше місто:")
+
+alert(`Ваша інформація:
+    Ваше ім'я: ${userName}
+    Ваш вік: ${year}
+    Ваше місто:${city}`);
