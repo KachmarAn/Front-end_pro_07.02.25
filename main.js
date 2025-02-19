0e3237455766ab3df3164483fb033ce813a1ec5e
KachmarAN <bilux.teplo@gmail.com>
2025-02-19 21:01:16 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 // Введіть п'ятизначне число
 let number = parseInt(prompt("Введіть п'ятизначне число:"));
@@ -11,3 +11,8 @@ let digit4 = (number % 100) / 10 - ((number % 100) / 10) % 1;
 let digit5 = number % 10;
 
 alert(`Digits: ${digit1} ${digit2} ${digit3} ${digit4} ${digit5}`);
+
+
+
+let number1 = prompt("Введіть тризначне число:");
+console.log("Зворотній порядок:", number1[2], number1[1], number1[0]);
