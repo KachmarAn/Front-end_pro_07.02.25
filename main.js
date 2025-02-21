9b96f6c29150f3b6d8eaf4ce9e9292273e4162a4
KachmarAn <bilux.teplo@gmail.com>
2025-02-21 20:40:04 +0200

    update main.js

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-let name = prompt("Введіть ваше ім'я:");
-alert(`Hello, ${name}. How are you?`);
+// Чи всі цифри тризначного числа однакові?
+let number = prompt ("Введіть тризначне число")
+
+// Виділяємо цифри
+let hundreds = (number / 100) | 0; // Сотні
+let tens = ((number % 100) / 10) | 0; // Десятки
+let ones = number % 10; // Одиниці
+
+// Перевірка
+if (hundreds === tens && tens === ones) {
+  alert("Усі цифри числа однакові.");
+} else if (hundreds === tens || hundreds === ones || tens === ones) {
+  alert("Є однакові цифри.");
+} else {
+  alert("Усі цифри різні.");
+}
