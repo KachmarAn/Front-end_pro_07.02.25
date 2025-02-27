b6b65ae45a1449f59dcad4ccb9bc8426e4b16fe7
KachmarAN <bilux.teplo@gmail.com>
2025-02-27 20:03:53 +0200

    commit

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+'use strict'
+
+const N = +prompt("Введіть ціле число N: "); // Унарний плюс конвертує в число
+
+for (let i = 1; i <= 100; i++) {
+    if (i * i <= N) {
+        console.log(i);
+    } 
+}
+
