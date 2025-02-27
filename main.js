25cc09f7f7fb294b557d1c4e0b0d86d010e29177
KachmarAN <bilux.teplo@gmail.com>
2025-02-27 19:48:39 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 "use strict";
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
 
-// Введіть п'ятизначне число
-let number = parseInt(prompt("Введіть п'ятизначне число:"));
-// Отримуємо цифри
-let digit1 = number / 10000 - (number / 10000) % 1;
-let digit2 = (number % 10000) / 1000 - ((number % 10000) / 1000) % 1;
-let digit3 = (number % 1000) / 100 - ((number % 1000) / 100) % 1;
-let digit4 = (number % 100) / 10 - ((number % 100) / 10) % 1;
-let digit5 = number % 10;
-alert(`Digits: ${digit1} ${digit2} ${digit3} ${digit4} ${digit5}`);
+switch (true) {
+case numOrStr === null:
+console.log('ви скасували');
+break;
+
+case numOrStr.trim() === '':
+console.log('Empty String');
+break;
+
+case isNaN(+numOrStr):
+    console.log('number is Ba_NaN');
+    break;
+
+default:
+    console.log('OK!');
 
 
-let number1 = prompt("Введіть тризначне число:");
-console.log("Зворотній порядок:", number1[2], number1[1], number1[0]);
+}
+// if (numOrStr === null) {
+//     console.log('ви скасували')
+// } else if (numOrStr.trim() === '') {
+//     console.log('Empty String');
+// } else if (isNaN(+numOrStr)) {
+//     console.log(' number is Ba_NaN')
+// } else {
+//     console.log('OK!')
+// }
