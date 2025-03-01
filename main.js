145e98377c66227c56d186b532c6380d3edd16b1
KachmarAN <bilux.teplo@gmail.com>
2025-03-01 13:58:39 +0200

    commit

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-function removeChars(str, charsToRemove) {
-    return str.split('').filter(char => !charsToRemove.includes(char)).join('');
+function averageOfNumbers(arr) {
+    // Фільтруємо тільки числові значення
+    let num = arr.filter(item => typeof item === 'number');
+
+    // Якщо числових значень немає, повертаємо null або 0
+    if (num.length === 0) return null;
+
+    // Обчислюємо суму чисел
+    let sum = num.reduce((acc, num) => acc + num, 0);
+
+    // Обчислюємо середнє арифметичне
+    return sum / num.length;
 }
 
-// Отримуємо вхідні дані від користувача
-const inputString = prompt("Введіть рядок:");
-const charsToRemove = prompt("Введіть символи для видалення без пробілів:").split('');
-
-// Викликаємо функцію та виводимо результат
-const result = removeChars(inputString, charsToRemove);
-console.log("Результат:", result);
+// Приклад використання
+const mixedArray = [50, "hello", 70, null, 10, "rumba", 40, false];
+console.log(averageOfNumbers(mixedArray)); // Виведе результат
