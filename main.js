d6170e020b97ec21749df3142efd98dafe8d3012
KachmarAN <bilux.teplo@gmail.com>
2025-03-09 16:47:11 +0200

    commit

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 'use strict';
-function multiply(a) {
-    return function(b) {
-        return a * b;
-    };
+function askNum() {
+    let num1;
+    let att = 10;
+
+    for (let i = 0; i < att; i++) {
+        num1 = prompt("Введіть число більше 100:");
+
+        // Перевіряємо, чи це число і чи воно більше 100
+        if (num1 !== null && !isNaN(num1) && Number(num1) > 100) {
+            console.log("Останнє введене число:", num1);
+            return;
+        }
+    }
+    console.log("Ліміт спроб вичерпано. Останнє введене число:", num1);
 }
 
-const result = multiply(5)(2);
-console.log (result);
+askNum();
