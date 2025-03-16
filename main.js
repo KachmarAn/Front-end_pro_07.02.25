69dc894e5e57dc84684b8d68f2798aee61be8459
KachmarAN <bilux.teplo@gmail.com>
2025-03-16 12:15:17 +0200

    commit

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
 'use strict';
 
-const ladder = {
-    step: 0,
-
-    up: function () {
-        this.step++;
-        return this;
-    },
-
-    down: function () {
-        this.step--;
-        return this;
-    },
-
-    showStep: function () {
-        console.log(this.step);
-        return this;
+let company = {
+    sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 600}],
+    development: {
+        web: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1000}],
+        internal: [{name: 'Jack', salary: 1300}]
     }
 };
-ladder.up().up().down().up().up().down().up().up().down().up().up().down().showStep();
+
+function sumSalaries(department) {
+    if (Array.isArray(department)) {
+        return department.reduce((sum, employee) => sum + employee.salary, 0);
+    } else {
+        return Object.values(department).reduce((sum, subdep) => sum + sumSalaries(subdep), 0);
+    }
+}
+
+console.log(sumSalaries(company));
