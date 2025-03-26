966ef82bca228639b5095b7378425b7de1fca580
KachmarAN <bilux.teplo@gmail.com>
2025-03-26 19:51:05 +0200

    commit

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const container = document.getElementById('button-container');
 container.addEventListener('click', function(event) {
     if (event.target.tagName === 'BUTTON') {
         const buttonName = event.target.getAttribute('data-name');
-        alert(`Ви натиснули: ${buttonName}`);
-        console.log(`Ви натиснули: ${buttonName}`)
+        alert(`Клікнуто на кнопці: ${buttonName}`);
+        console.log(`Клікнуто на кнопці: ${buttonName}`)
     }
 });
