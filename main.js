09c79ae6038ce249ec30a0b72a8c64db1c15cfef
KachmarAN <bilux.teplo@gmail.com>
2025-04-09 20:19:44 +0300

    commit

--- main.js
+++ main.js
@@ -1,63 +1,32 @@
 'use strict';
 
-function Student(firstName, lastName, birthYear) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.birthYear = birthYear;
-    this.grades = [];
-    this.attendance = new Array(25).fill(null);
+class Calculator {
+    add(a, b) {
+        return a + b;
+    }
+
+    subtract(a, b) {
+        return a - b;
+    }
+
+    multiply(a, b) {
+        return a * b;
+    }
+
+    divide(a, b) {
+        if (b === 0) {
+            throw new Error("Ділення на нуль неможливе");
+        }
+        return a / b;
+    }
 }
 
-Student.prototype.getAge = function() {
-    return new Date().getFullYear() - this.birthYear;
-};
+const calc = new Calculator();
 
-Student.prototype.getAverageGrade = function() {
-    if (this.grades.length === 0) return 0;
-    return this.grades.reduce((sum, grade) => sum + grade, 0) / this.grades.length;
-};
+console.log(calc.add(5, 3)); // 8
 
-Student.prototype.present = function() {
-    const index = this.attendance.indexOf(null);
-    if (index !== -1) {
-        this.attendance[index] = true;
-    } else {
-        console.log("Всі заняття вже заповнені.");
-    }
-};
+console.log(calc.subtract(10, 4)); // 6
 
-Student.prototype.absent = function() {
-    const index = this.attendance.indexOf(null);
-    if (index !== -1) {
-        this.attendance[index] = false;
-    } else {
-        console.log("Всі заняття вже заповнені.");
-    }
-};
+console.log(calc.multiply(3, 6)); // 18
 
-Student.prototype.getAttendanceRate = function() {
-    const lessons = this.attendance.filter(item => item !== null);
-    if (lessons.length === 0) return 0;
-    const present = lessons.filter(item => item === true).length;
-    return present / lessons.length;
-};
-
-Student.prototype.summary = function() {
-    const avgGrade = this.getAverageGrade();
-    const attendanceRate = this.getAttendanceRate();
-    if (avgGrade > 90 && attendanceRate > 0.9) {
-        return "Молодець!";
-    } else if (avgGrade > 90 || attendanceRate > 0.9) {
-        return "Добре, але можна краще";
-    } else {
-        return "Редиска!";
-    }
-};
-
-const student1 = new Student("Василь", "Петренко", 2000);
-student1.grades.push(98, 95, 92);
-student1.present();
-student1.present();
-student1.present();
-
-console.log(student1.summary()); // Молодець!
+console.log(calc.divide(8, 2)); // 4
