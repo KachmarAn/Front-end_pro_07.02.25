59ac3656325b04e315befa9262c860539ed0e2f7
KachmarAN <bilux.teplo@gmail.com>
2025-04-09 20:36:37 +0300

    commit

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
 'use strict';
 
-class Coach {
-constructor(name, specialization, rating) {
-    this.name = name;
-    this.specialization = specialization;
-    this.rating = rating;
+class BankAccount {
+    constructor(initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    getBalance() {
+        return this.balance;
+    }
+
+    deposit(amount) {
+        if (amount > 0) {
+            this.balance += amount;
+        } else {
+            console.log("Сума для внесення має жбути більшою за 0")
+        }
+    }
+
+    withdraw(amount) {
+        if (amount > 0 && amount <= this.balance) {
+            this.balance -= amount;
+        } else {
+            console.log("Недостатньо коштів")
+        }
+    }
 }
-displayInfo() {
-    console.log(`Coach:  ${this.name},  Specialization: ${this.specialization}, Rating: ${this.rating}\n`);
-}
-}
 
-const coach1 = new Coach('John Doe', 'Fitness', 4.7);
+const account1 = new BankAccount(1000);
 
-const coach2 = new Coach('Alice Smith', 'Yoga', 4.9);
+console.log(account1.getBalance()); // 1000
 
-coach1.displayInfo(); // "Coach: John Doe, Specialization: Fitness, Rating: 4.7"
+account1.deposit(500);
 
-coach2.displayInfo(); // "Coach: Alice Smith, Specialization: Yoga, Rating: 4.9"
+console.log(account1.getBalance()); // 1500
+
+account1.withdraw(200);
+
+console.log(account1.getBalance()); // 1300
